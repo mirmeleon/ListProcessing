2728e72aafbdfa736245f646b81a74946ce0a24c
mirmeleon <morgensterndenise@gmail.com>
2017-07-14 20:42:54 +0200

    added theArray

--- consoleReader.js
+++ consoleReader.js
@@ -1,11 +1,21 @@
 (function (){
+   let theArray = [];
+   let initialized = false;
    let terminal = document.getElementById('terminal');
    let input = document.getElementById('console');
    document.getElementById('submit').addEventListener('click', submit);
    input.addEventListener('keypress', (e) => e.code ==='Enter' ? submit() : '');
 
    function submit(){
+      if(!initialized){
+         theArray = commandTokens.slice(0);
+         return;
+      }
        let commandTokens = input.value().split(' ').filter(e => e !=='');
+       switch(commandTokens[0]){
+           //todo
+
+       }
 
    }
 })();
